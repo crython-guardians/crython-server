c7ffb02a43091cfc351eb61e50a94cf507747dbf
quro <hs970216@naver.com>
2024-08-06 00:47:45 +0900

    feat: project 기본 세팅

---
+++ src/main/java/crypton/CryptoGuardians/CryptoGuardiansApplication.java
@@ -0,0 +1,13 @@
+package crypton.CryptoGuardians;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoGuardiansApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptoGuardiansApplication.class, args);
+	}
+
+}
---
+++ src/test/java/crypton/CryptoGuardians/CryptoGuardiansApplicationTests.java
@@ -0,0 +1,13 @@
+package crypton.CryptoGuardians;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoGuardiansApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
