f8cf452904fb5dd387f9cec2a1ab98e2cace8698
chaewon12 <djacltk@naver.com>
2024-08-07 18:48:27 +0900

    fix: AuthorizeResponseDTO record 타입으로 변경(#5)

--- src/main/java/crypton/CryptoGuardians/domain/document/dto/AuthorizeResponseDTO.java
+++ src/main/java/crypton/CryptoGuardians/domain/document/dto/AuthorizeResponseDTO.java
@@ -1,12 +1,4 @@
 package crypton.CryptoGuardians.domain.document.dto;
 
-import lombok.Getter;
-
-@Getter
-public class AuthorizeResponseDTO {
-    private final String key;
-
-    public AuthorizeResponseDTO(String key) {
-        this.key = key;
-    }
+public record AuthorizeResponseDTO(String key) {
 }
