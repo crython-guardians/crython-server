3c6550c0e49122698858ebedc83b2a5e14202b6b
chaewon12 <djacltk@naver.com>
2024-08-07 17:08:01 +0900

    feat: 문서 키 테이블 DocumentKey 생성 (#5)

---
+++ src/main/java/crypton/CryptoGuardians/domain/document/entity/DocumentKey.java
@@ -0,0 +1,31 @@
+package crypton.CryptoGuardians.domain.document.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Entity
+@Table(name = "document_key_tb")
+public class DocumentKey {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "document_id")
+    private Document document;
+
+    private String key;
+
+    public static DocumentKey createDocumentKey(Document document){
+        DocumentKey documentKey = new DocumentKey();
+        documentKey.document = document;
+        documentKey.key = UUID.randomUUID().toString();
+        return documentKey;
+    }
+}
---
+++ src/main/java/crypton/CryptoGuardians/domain/document/repository/DocumentKeyRepository.java
@@ -0,0 +1,13 @@
+package crypton.CryptoGuardians.domain.document.repository;
+
+import crypton.CryptoGuardians.domain.document.entity.Document;
+import crypton.CryptoGuardians.domain.document.entity.DocumentKey;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface DocumentKeyRepository extends JpaRepository<DocumentKey, Long> {
+    Optional<DocumentKey> findByDocumentId(Long id);
+}
