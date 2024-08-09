aefe6de295454f9361db085add2f0592729c475e
chaewon12 <djacltk@naver.com>
2024-08-09 18:29:22 +0900

    fix: document - documentKey 관계 수정에 따른 비지니스 로직 변경

--- src/main/java/crypton/CryptoGuardians/domain/document/service/DocumentServiceImpl.java
+++ src/main/java/crypton/CryptoGuardians/domain/document/service/DocumentServiceImpl.java
@@ -118,9 +118,8 @@ public class DocumentServiceImpl implements DocumentService {
 
     @Override
     public AuthorizeResponseDTO getAuthorizeKey(Long documentId) {
-        DocumentKey documentKey = documentKeyRepository.findByDocumentId(documentId).orElseThrow(() -> new Exception404("파일 인증 키를 찾을 수 없습니다."));
-        String key = documentKey.getAuthKey();
-        return new AuthorizeResponseDTO(key);
+        Document document = documentRepository.findById(documentId).orElseThrow(() -> new Exception404("파일을 찾을 수 없습니다."));
+        return new AuthorizeResponseDTO(document.getDocumentKey().getAuthKey());
     }
 
     @Override
