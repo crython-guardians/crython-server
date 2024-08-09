86e640bf11d3e4cbf2ff5e17ccaa8418fa0d1542
Seung Ju Lee <127167006+SeungJuL@users.noreply.github.com>
2024-08-09 09:44:05 -0400

    오타수정

--- src/main/java/crypton/CryptoGuardians/domain/user/api/UserController.java
+++ src/main/java/crypton/CryptoGuardians/domain/user/api/UserController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/users")
+@RequestMapping("/api/user")
 public class UserController {
     private final UserService userService;
 
