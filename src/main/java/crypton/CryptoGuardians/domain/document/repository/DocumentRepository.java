68ec976a450f8484add2246ec2a6e067ad5bd014
chaewon12 <djacltk@naver.com>
2024-08-11 22:03:55 +0900

    feat: 파일 검색 기능 구현 (#24)

--- src/main/java/crypton/CryptoGuardians/domain/document/repository/DocumentRepository.java
+++ src/main/java/crypton/CryptoGuardians/domain/document/repository/DocumentRepository.java
@@ -15,4 +15,9 @@ public interface DocumentRepository extends JpaRepository<Document, Long> {
     @Query("SELECT d FROM Document d left join DocumentShare ds on d.id = ds.fileId.id " +
             "WHERE d.uploadUser.id = :userId or ds.sharedUserId = :userId ORDER BY d.createdAt ASC")
     List<Document> findByUploadUserIdOrderByCreatedAtAsc(@Param("userId") Long userId);
+
+    @Query("SELECT d FROM Document d " +
+            "WHERE d.uploadUser.id = :userId AND d.fileName LIKE %:keyword% " +
+            "ORDER BY d.createdAt ASC")
+    List<Document> searchDocumentOrderByCreatedAtAsc(Long userId, String keyword);
 }
